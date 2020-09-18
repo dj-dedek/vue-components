6013706fa27a1ab6f808b073472e035b12fd279c
dj-dedek <dedekitsjay@gmail.com>
2020-09-19 04:17:26 +0700

    init commit

---
+++ src/components/User.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my Name</button>
+        <p>Name is {{ name }}</p>
+        <p>Age  is {{ age }}</p>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail 
+                    :myName="name" 
+                    @nameWasReset="name = $event"
+                    :resetFn="resetName"
+                    :userAge="age"
+                >
+                </app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit 
+                    :userAge="age"
+                    @ageWasEdited="age = $event"
+                    ></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import UserDetail from './UserDetail';
+    import UserEdit from './UserEdit';
+
+    export default {
+        data: function() {
+            return {
+                name: 'Dedek',
+                age: 20
+            }
+        },
+        methods: {
+            changeName() {
+                this.name = 'Rizka'
+            },
+            resetName() {
+                this.name = 'Dedek'
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
